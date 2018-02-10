40c97faeea4266d6e926a31a56c1fb1064c77a56
Unknown <ichigocruces@gmail.com>
2018-02-10 20:33:45 +0100

    Izena as fragment

--- app/src/main/java/eus/arabyte/android/izenpedia/activity/fragments/IzenaFragment.java
+++ app/src/main/java/eus/arabyte/android/izenpedia/activity/fragments/IzenaFragment.java
@@ -1,16 +1,20 @@
-package eus.arabyte.android.izenpedia.activity;
+package eus.arabyte.android.izenpedia.activity.fragments;
+
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.text.Html;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
@@ -29,7 +33,10 @@ import eus.arabyte.android.izenpedia.model.Izenkidea;
 import eus.arabyte.android.izenpedia.utils.Constants;
 import eus.arabyte.android.izenpedia.utils.Preferences;
 
-public class IzenaActivity extends AppCompatActivity {
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class IzenaFragment extends Fragment {
 
     public static final String ARG_IZENA = "izena";
 
@@ -42,56 +49,78 @@ public class IzenaActivity extends AppCompatActivity {
 
     private IzenkideaAdapter izenkediaAdapter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_izena);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        preferences = Preferences.getInstance(this);
+    public IzenaFragment() {
+        // Required empty public constructor
+        super();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.content_izena, container, false);
+
+        preferences = Preferences.getInstance(rootView.getContext());
+
 
         // cargar los datos
-        Bundle args = getIntent().getExtras();
+//        Bundle args = getIntent().getExtras();
+        Bundle args = getArguments();
         String argIzena = null;
-        if(args!=null) {
+        if (args != null) {
             argIzena = args.getString(ARG_IZENA);
         }
 
-        izenaDAO = new IzenaDAOImpl(this);
+        izenaDAO = new IzenaDAOImpl(rootView.getContext());
         izena = izenaDAO.getIzena(argIzena);
-        setTitle(izena.getIzena());
+        getActivity().setTitle(izena.getIzena());
 
         String description = izena.getAzalpenaEu();
-        if(Constants.ES.equals(preferences.getHizkuntza())){
+        if (Constants.ES.equals(preferences.getHizkuntza())) {
             description = izena.getAzalpenaEs();
         }
 
-        ((TextView)this.findViewById(R.id.izena_description))
+        ((TextView) rootView.findViewById(R.id.izena_description))
                 .setText(Html.fromHtml(description));
 
-        izenkideaDAO = new IzenkideaDAOImpl(this);
+        izenkideaDAO = new IzenkideaDAOImpl(rootView.getContext());
         List<Izenkidea> izenkideaList = izenkideaDAO.getListIzenkidea(izena);
 
         izenkediaAdapter = new IzenkideaAdapter(izenkideaList);
-        RecyclerView listIzenkideakView = this.findViewById(R.id.list_izenkideak);
+        RecyclerView listIzenkideakView = rootView.findViewById(R.id.list_izenkideak);
         listIzenkideakView.setAdapter(izenkediaAdapter);
-        listIzenkideakView.setLayoutManager(new LinearLayoutManager(this));
+        listIzenkideakView.setLayoutManager(new LinearLayoutManager(rootView.getContext()));
 
 
-        gogokoaDAO = new GogokoaDAOImpl(this);
+        gogokoaDAO = new GogokoaDAOImpl(rootView.getContext());
+
+        return rootView;
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_izena, menu);
-
-        return true;
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 
+
     @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        menu.clear();
+        inflater.inflate(R.menu.menu_izena, menu);
+    }
+
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
         MenuItem menuItem = menu.findItem(R.id.action_star);
         final CheckBox starMenu = (CheckBox)menuItem.getActionView();
 
@@ -121,9 +150,6 @@ public class IzenaActivity extends AppCompatActivity {
 
             }
         });
-
-
-        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
@@ -149,6 +175,4 @@ public class IzenaActivity extends AppCompatActivity {
         }
 
     }
-
-
 }
