7348ce264f5c756a3c5f5fb09402944513139889
Unknown <ichigocruces@gmail.com>
2018-04-22 18:56:45 +0200

    Translations. Added property file for the URL

--- app/src/main/java/eus/arabyte/android/izenpedia/activity/fragments/AboutFragment.java
+++ app/src/main/java/eus/arabyte/android/izenpedia/activity/fragments/AboutFragment.java
@@ -1,20 +1,25 @@
 package eus.arabyte.android.izenpedia.activity.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import eus.arabyte.android.izenpedia.BuildConfig;
 import eus.arabyte.android.izenpedia.R;
+import eus.arabyte.android.izenpedia.utils.PropertiesReader;
+import eus.arabyte.android.izenpedia.utils.Utils;
 
 /**
  * Created by ichigo on 15/4/18.
  */
-public class AboutFragment extends Fragment {
+public class AboutFragment extends Fragment implements View.OnClickListener {
 
     public AboutFragment() {
     }
@@ -27,12 +32,27 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView =  inflater.inflate(R.layout.content_about, container, false);
+        View rootView = inflater.inflate(R.layout.content_about, container, false);
 
         String versionName = BuildConfig.VERSION_NAME;
 
         TextView versionText = rootView.findViewById(R.id.version_text);
         versionText.setText(versionName);
+
+        Button urlButton = rootView.findViewById(R.id.inigo_linkedin);
+        urlButton.setOnClickListener(this);
+
+        urlButton = rootView.findViewById(R.id.inigo_github);
+        urlButton.setOnClickListener(this);
+
+        urlButton = rootView.findViewById(R.id.ibai_linkedin);
+        urlButton.setOnClickListener(this);
+
+        urlButton = rootView.findViewById(R.id.ibai_github);
+        urlButton.setOnClickListener(this);
+
+        TextView urlText = rootView.findViewById(R.id.rate_text);
+        urlText.setOnClickListener(this);
 
         return rootView;
 
@@ -41,7 +61,7 @@ public class AboutFragment extends Fragment {
     /**
      * Método al que se invoca cuando se ha creado la vista
      *
-     * @param view View
+     * @param view               View
      * @param savedInstanceState Bundle
      */
     @Override
@@ -51,4 +71,42 @@ public class AboutFragment extends Fragment {
         getActivity().setTitle(R.string.nav_about);
     }
 
+    /**
+     * Open a webpage on the browser
+     *
+     * @param view View
+     */
+    @Override
+    public void onClick(final View view) {
+        String url = null;
+
+        PropertiesReader propertiesReader = PropertiesReader.getInstance(getContext());
+
+        switch (view.getId()) {
+            case R.id.inigo_linkedin:
+                url = propertiesReader.getInigoLinkedin();
+                break;
+            case R.id.inigo_github:
+                url = propertiesReader.getInigoGithub();
+                break;
+            case R.id.ibai_linkedin:
+                url = propertiesReader.getIbaiLinkedin();
+                break;
+            case R.id.ibai_github:
+                url = propertiesReader.getIbaiGithub();
+                break;
+            case R.id.rate_text:
+                url = propertiesReader.getUrlGoogleplay();
+                break;
+            default:
+                break;
+
+        }
+
+        if(!Utils.isBlank(url)){
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(browserIntent);
+        }
+
+    }
 }
